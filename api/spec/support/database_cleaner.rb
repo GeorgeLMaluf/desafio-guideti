4425a5d179975203f0f0a51765d759f307edfacd
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2022-11-11 17:57:02 -0300

    Adicionado RSpec ao projeto Rails

---
+++ api/spec/support/database_cleaner.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  DatabaseCleaner.allow_remote_database_url = true
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
